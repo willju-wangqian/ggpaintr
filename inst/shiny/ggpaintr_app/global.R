7a2314cbb57b30a0fb043eb85669cb78dcdab859
willju-wangqian <wju@iastate.edu>
2022-04-30 21:12:15 -0500

    added more updates for version 0.0.9.6; expected to be stable

--- inst/shiny/ggpaintr_app/global.R
+++ inst/shiny/ggpaintr_app/global.R
@@ -1,29 +1,20 @@
-library(tidyverse)
 library(palmerpenguins)
-library(shinyWidgets)
 library(shiny)
 library(DT)
 library(shinydashboard)
-library(colourpicker)
-library(RColorBrewer)
-library(assertthat)
-
+library(shinyBS)
 library(ggpaintr)
 
-# library(shinyFiles)
-# library(shinybusy)
-# library(shinyAce)
-# library(formatR)
-# library(shinyhelper)
-library(shinyBS)
-
-# setwd('C:/Downloads/project/ggpaintr/inst/shiny/ggpaintr_app/')
+# library(tidyverse)
+# library(shinyWidgets)
+# library(colourpicker)
+# library(RColorBrewer)
+# library(assertthat)
 
 # sapply(list.files("R_funcs/"), function(fileName) {
 #   source(paste0("R_funcs/", fileName))
 # })
 
-# data("penguins")
 
 DODGE_WID_DEFAULT <- 0.5
 myStatUI <- function(x, y) { h3(paste(x + y)) }
---
+++ preconsideration/demo/ggpaintr_demo/app.R
@@ -0,0 +1,116 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggpaintr)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("ggpaintr demo"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            pickerInput("defaultData", "select a default dataset:",
+                        choices = c("iris", "mtcars","penguins", "faithfuld"),
+                        selected = "",
+                        multiple = TRUE,
+                        options = pickerOptions(maxOptions = 1)),
+            uiOutput("someUI"),
+            actionButton("draw", "label: draw")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("mainPlot"),
+            verbatimTextOutput('mycode')
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    box_control_id <- "boxControl"
+
+    ns_box <- NS(box_control_id)
+
+    # data
+    data_container <- reactive({
+        req(input$defaultData)
+        get(input$defaultData)
+    })
+
+    # construct paintr object
+    box_main <- reactive({
+
+        req(data_container())
+        data <- data_container()
+
+        paintr(
+            id = box_control_id,
+            data = data,
+            data_path = input$defaultData,
+            # expr = geom_point(aes(x, y, color), alpha) +
+            #     theme_choose +
+            #     labs(x, y, title)
+            expr = geom_contour(aes(x,y,z))
+        )
+
+    })
+
+    # place ui
+    output$someUI <- renderUI({
+        req(box_main())
+
+        column(
+            12,
+            paintr_get_ui(box_main(), "x"),
+            paintr_get_ui(box_main(), "y"),
+            paintr_get_ui(box_main(), "z"),
+            # paintr_get_ui(box_main(), "color"),
+            # paintr_get_ui(box_main(), "fill"),
+            # paintr_get_ui(box_main(), "alpha"),
+            # paintr_get_ui(box_main(), "labs"),
+            # paintr_get_ui(box_main(), "theme_choose"),
+            # paintr_get_ui(box_main(), "theme"),
+            # paintr_get_ui(box_main(), "position"),
+            # paintr_get_ui(box_main(), "facet_grid")
+            # ...
+        )
+    })
+
+    # take results and plot
+    observe({
+        req(box_main(), data_container(), input$defaultData)
+        data <- data_container()
+
+        paintr_list <- paintr_plot_code(box_main())
+
+        output$mainPlot <- renderPlot({
+
+            validate(need(paintr_list[['plot']], "plot is not rendered"))
+
+            paintr_list[['plot']]
+        })
+
+        output$mycode <- renderText({
+
+            paintr_list[['code']]
+
+        })
+
+    }) %>% bindEvent(input$draw)
+
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
